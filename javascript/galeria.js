95d412a3ec5b0004d0168abded48beec8ae4c2a1
Renzo <81322658+RenzoMarangon@users.noreply.github.com>
2022-03-05 15:58:57 -0300

    v1.2

--- javascript/galeria.js
+++ javascript/galeria.js
@@ -1,3 +1,27 @@
+const menuHamburguesa = document.querySelector(".header-container__menu-responsive");
+const linkContainer = document.querySelector(".header-container__link");
+const header = document.querySelector("header");
+const main = document.querySelector("main");
+
+
+let mostrarocultar=false;
+menuHamburguesa.addEventListener("click",()=>{
+    if(!mostrarocultar){
+        linkContainer.style.height=linkContainer.scrollHeight+"px";
+        mostrarocultar=true;
+    }else{
+        linkContainer.style.overflow="hidden"
+        linkContainer.style.height=0;
+        mostrarocultar=false;
+    }
+   
+    menuHamburguesa.classList.toggle("click-menu-hamburguesa");
+    setTimeout(()=>{
+        menuHamburguesa.classList.toggle("click-menu-hamburguesa");
+    },200)
+    
+})
+
 
 const animados = document.querySelectorAll(".animacion");
 animados[0].classList.add("animacionIzquierda");
